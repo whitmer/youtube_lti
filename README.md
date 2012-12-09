5d30112e34ee8fe0d52cdee10cfd9cd5e5e49330
Brian Whitmer <brian@instructure.com>
2012-12-08 17:07:36 -0700

    minor tweaks

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ YouTube Search LTI App
 ---------------------------
 This is a really basic app that will let teachers search for
 and set links to YouTube videos within their course content.
+
+More information available here:  http://brianwhitmer.blogspot.com/2012/12/building-youtube-lti-app.html
+
+### Setting Up
+
+- Make sure ruby is installed
+- gem install bundler
+- bundle install
+- ruby youtube_lti.rb
+- The server should now be running at http://localhost:4567
+- Set the LTI launch URL to http://loocalhost:4567/lti_launch
--- youtube_lti.rb
+++ youtube_lti.rb
@@ -18,9 +18,9 @@ require 'sinatra'
 require 'dm-core'
 require 'dm-migrations'
 
-# sinatra wants to set x-frame-options by default, disable it
+# Sinatra wants to set x-frame-options by default, disable it
 disable :protection
-# enable sessions so we can remember the launch info between http requests, as
+# Enable sessions so we can remember the launch info between http requests, as
 # the user takes the assessment
 enable :sessions
 
@@ -38,15 +38,17 @@ post "/lti_launch" do
       params['tool_consumer_instance_guid']
   placement = Placement.first(:placement_id => placement_id)
   if placement
-    # use /embed instead of /v or it won't work in an iframe
+    # Use /embed instead of /v or it won't work in an iframe.
+    # It's also a good idea to use https whenever possible, since some
+    # platforms (Canvas included) run everything over SSL.
     redirect to "https://youtube.com/embed/"  + placement.video_id
   else
     # use a cookie-based session to remember placement permission
     session["can_set_" + placement_id] = true
 
-    # let the user pick the video to use for this placement
-    # if you want to make sure students don't pick a video before the teacher
-    # can get to this placement, you would check the "roles" parameter
+    # Let the user pick the video to use for this placement.
+    # If you want to make sure students don't pick a video before the teacher
+    # can get to this placement, you would check the "roles" parameter.
     redirect to ("/youtube_search.html?placement_id=" + placement_id)
   end
 end
